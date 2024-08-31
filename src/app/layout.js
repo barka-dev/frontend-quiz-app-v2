e0a2e486a805dbc45bd5a6f7056149ac527cddb2
Abdel <abdelbarka96@gmail.com>
2024-08-31 16:30:13 +0100

    next js setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <img src="/images/icon-accessibility.svg" alt="topic icon"/>
+          <h1>Accessibility</h1>
+          <img src="/images/icon-sun-dark.svg" class="sunmoonIcons" alt="sun icon"/>
+          <input type="checkbox" name="theme" class="themeIcon" id="theme"/>
+          <img src="/images/icon-moon-dark.svg" class="sunmoonIcons" alt="moon icon"/>
+        </header>
+        {children}
+        </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,30 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+    <main>
+      <section>
+        <h2>Welcome to the</h2>
+        <strong>Frontend Quiz!</strong>
+        <p>Pick a subject to get started.</p>
+      </section>
+      <article>
+        <a href="/html_section" className="subjects">
+          <img src="/images/icon-html.svg" alt="html icon" /> HTML
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        <a href="/CSS_section" className="subjects">
+          <img src="/images/icon-css.svg" alt="css icon" /> CSS
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
+        <a href="/html_section" className="subjects">
+          <img src="/images/icon-js.svg" alt="js icon" /> Javascript
         </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+        <a href="/html_section" className="subjects">
+          <img
+            src="/images/icon-accessibility.svg"
+            alt="accessibility icon"
+          />
+          Accessibility
         </a>
-      </div>
+      </article>
     </main>
   );
 }
