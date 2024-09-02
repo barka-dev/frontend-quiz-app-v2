defffa5e025fac8d96a4857e6d565a8b0a7265ad
Abdel <abdelbarka96@gmail.com>
2024-09-02 14:02:37 +0100

    question / answer layout static version

--- src/app/components/answersSection.js
+++ src/app/components/answersSection.js
@@ -3,18 +3,27 @@ import Image from "next/image";
 export default function AnswersSection(){
     return(
         <>
-            <a href="/questions" className="subjects" >
-                <Image src="/images/icon-html.svg" alt="html icon"  width={40} height={40}/> HTML
-            </a>
-            <a href="#" className="subjects">
-                <Image src="/images/icon-css.svg" alt="css icon"  width={40} height={40} /> CSS
-            </a>
-            <a href="#" className="subjects">
-                <Image src="/images/icon-js.svg" alt="js icon"  width={40} height={40} /> Javascript
-            </a>
-            <a href="#" className="subjects">
-                <Image src="/images/icon-accessibility.svg" alt="accessibility icon" width={40} height={40}/> Accessibility
-            </a>
+            <label for="answer_A" className="answers">
+                <input type="radio" id="answer_A" name="answer" value="A"/> 
+                <span>A</span>
+                4.5:1
+            </label>
+            <label for="answer_B" className="answers">
+                <input type="radio" id="answer_B" name="answer" value="B"/> 
+                <span>B</span>
+                3:1
+            </label>
+            <label for="answer_c" className="answers">
+                <input type="radio" id="answer_c" name="answer" value="C"/> 
+                <span>C</span>
+                2.5:1
+            </label>
+            <label for="answer_d" className="answers">
+                <input type="radio" id="answer_d" name="answer" value="D"/> 
+                <span>D</span>
+                5:1
+            </label>
+            <button className="submit_answer">Submit Answer</button>
         </>
     )
 }
